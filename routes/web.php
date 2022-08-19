d6e76b4d0a777b4ade13a90bbe45742d155fe041
devScosta <dev.scosta@gmail.com>
2022-08-19 13:15:22 -0300

    Redirecioamento

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use function Ramsey\Uuid\v1;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,6 +14,52 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/view', function () {
+    return view('welcome');
+});
+
+Route::redirect('/redirect1', '/redirect2');
+
+//Route::get('redirect1', function() {
+//    return redirect('/redirect2');
+//});
+
+Route::get('redirect2', function() {
+    return 'Redirect 02';
+});
+
+Route::get('/produtos/{idProduct?}', function($idProduct = '') {
+    return "PRODUTO(S) {$idProduct}";
+});
+
+Route::get('/categorias/{flag}/posts', function($posts) {
+    return "Posts por categorias: {$posts}";
+});
+
+Route::get('/categorias/{flag}', function($flag) {
+    return "Produtos por categoria: {$flag}";
+});
+
+Route::match(['PUT','post'], '/march', function() {
+    return 'March';
+});
+
+Route::any('/any', function() {
+    return 'Any';
+});
+
+Route::post('/register', function() {
+    return '';
+});
+
+Route::get('/empresa', function() {
+    return view('site.contacto');
+});
+
+Route::get('/contato', function() {
+    return 'CONTATOS';
+});
 
 Route::get('/', function () {
     return view('welcome');
