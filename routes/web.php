7aa0d2941e0319874a3ced2cacefdba9843579c8
devScosta <dev.scosta@gmail.com>
2022-08-29 15:24:15 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 use function Ramsey\Uuid\v1;
 
@@ -14,8 +16,63 @@ use function Ramsey\Uuid\v1;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::view('/view', 'welcome');
 
+
+
+Route::get('/login', function() {
+    return 'login';
+})->name('login');
+
+/*Route::middleware([])->group(function() {
+    
+    Route::prefix('admin')->group(function() {
+
+        Route::namespace('App\Http\Controllers\Admin')->group(function () {
+            Route::name('admin.')->group(function() {
+                Route::get('/dashboard', 'TesteController@teste')->name('dashboard');
+            
+                Route::get('/financeiro', 'TesteController@teste')->name('finaceiro');
+                
+                Route::get('/produtos', 'TesteController@teste')->name('products');
+    
+                Route::get('/', function () {
+                    return redirect()->route('dashboard');
+                })->name('home');
+            });
+        }); 
+    });
+});*/
+
+Route::group([
+    'middleware' => [],
+    'prefix'=> 'admin',
+    'namespace'=> 'App\Http\Controllers\Admin',
+    'name'=> 'admin'
+], function() {
+        Route::get('/dashboard', 'TesteController@teste')->name('dashboard');
+                
+        Route::get('/financeiro', 'TesteController@teste')->name('financeiro');
+                    
+        Route::get('/produtos', 'TesteController@teste')->name('products');
+        
+        Route::get('/', function () {
+            return redirect()->route('dashboard');
+        })->name('home');
+});
+
+Route::get('/contacto', function() {
+    return 'HELLO WORD!';
+})->name('contatos.empresa');
+
+Route::get('redirect4', function() {
+    return redirect()->route('contatos.empresa');
+});
+
+Route::get('/name-url', function() {
+    return 'Hay, HOY, HOY';
+})->name('url.name');
+
+Route::view('/view', 'welcome');
 
 
 Route::redirect('/redirect1', '/redirect2');
@@ -26,6 +83,10 @@ Route::redirect('/redirect1', '/redirect2');
 
 Route::get('redirect2', function() {
     return 'Redirect 02';
+});
+
+Route::get('redirect3', function() {
+        return redirect()->route('url.name');
 });
 
 Route::get('/produtos/{idProduct?}', function($idProduct = '') {
